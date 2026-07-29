2ca3896496188b83785b089b489f56c8ef2aebe4
sayooojjayarajanuk <uksayoojjayarajan@gmail.com>
2026-07-29 22:01:24 +0530

    refactor: remove explicit EntityScan and EnableJpaRepositories annotations from main class

--- src/main/java/com/restaurant/pos/CafeQrBackendApplication.java
+++ src/main/java/com/restaurant/pos/CafeQrBackendApplication.java
@@ -2,9 +2,6 @@ package com.restaurant.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.retry.annotation.EnableRetry;
@@ -12,8 +9,6 @@ import org.springframework.retry.annotation.EnableRetry;
 @EnableAsync
 @EnableRetry(order = 99)
 @EnableScheduling
-@EntityScan("com.restaurant.pos")
-@EnableJpaRepositories("com.restaurant.pos")
 @SpringBootApplication(scanBasePackages = "com.restaurant.pos", exclude = {
     org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class
 })
