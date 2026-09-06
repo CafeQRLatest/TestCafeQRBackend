f118624c4eef0d401cb85790bf8f32d88c7ce500
Riyas <pnriyas50@gmail.com>
2026-09-07 04:36:17 +0530

    hr and payroll , salary rules 2

--- src/main/java/com/restaurant/pos/hr/dto/SalaryComponentDto.java
+++ src/main/java/com/restaurant/pos/hr/dto/SalaryComponentDto.java
@@ -1,5 +1,6 @@
 package com.restaurant.pos.hr.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +17,18 @@ public class SalaryComponentDto {
     private UUID id;
     private String name;
     private String type; // EARNING or DEDUCTION
+
+    @JsonProperty("isTaxApplicable")
     private boolean isTaxApplicable;
+
+    @JsonProperty("dependsOnAttendance")
     private boolean dependsOnAttendance;
+
     private String amountType; // FIXED or PERCENTAGE
     private BigDecimal defaultAmount;
     private BigDecimal percentage;
     private String percentageOfComponent;
-    private boolean isActive;
+
+    @JsonProperty("isActive")
+    private boolean isActive = true;
 }
